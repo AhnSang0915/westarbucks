5a1e540110bb6594a1db8e8fcb5bed1016e5766d
Ahnsang0915 <tmqjf4921@gmail.com>
2022-07-01 16:51:21 +0900

    CREATE: 프로젝트 초기 셋팅

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westarbucks',
+        'USER': 'root',
+        'PASSWORD': 'qhrjtm',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-y=+-o6!**%hpq06t9vn)nyyt1^dnk%q_f202*ex^z)ojzg$itt'
